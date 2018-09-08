0d37cb64851cc48ad2493b7f9b1ec1500513588b
AzizSudrajat <azizsudrajat95@gmail.com>
2018-09-08 16:56:41 +0700

    Update Footer

--- application/views/layout/footer_home.php
+++ application/views/layout/footer_home.php
@@ -1,44 +1,8 @@
 <!-- CONTENT-WRAPPER SECTION END-->
 <footer>
-  <div class="footer-top">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-        <h3><b>Tentang Kami</b></h3>
-        <p style="text-align: justify">
-          PT. Bando adalah salah satu produsen sabuk transmisi otomotif dan industri terkemuka di Indonesia. Ini memiliki satu pabrik utama yang berlokasi di Tangerang dan kantor pemasarannya berlokasi di Jakarta Pusat. Perusahaan ini didirikan pada tahun 1987
-         </p>
-
-        </div>
-        <div class="col-md-2 list">
-          <ul>
-            <li><a href="<?php echo base_url(); ?>">Beranda</a></li>
-            <li><a href="<?php echo base_url('kriteria'); ?>">Data Kriteria</a></li>
-            <li><a href="<?php echo base_url('nilai'); ?>">Data Nilai</a></li>
-            <li><a href="<?php echo base_url('alternatif'); ?>">Data Alternatif</a></li>
-          </ul>
-
-        </div>
-        <div class="col-md-2 list">
-          <ul>
-            <li><a href="<?php echo base_url('kriteria/analisa'); ?>">Analisis Kriteria</a></li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <h5><strong>Informasi Kontak</strong></h5>
-          <p><strong>Alamat :</strong> Jln. Gajah Tunggal Km. 7 Tangerang Kel. Pasir Jaya, Kecamatan Jati Uwung Kotamadya, Tangeran
-          </p>
-          <p><strong>Email :</strong> vbelt.div@bandoindonesia.com</p>
-          <p><strong>Phone :</strong> (021) 590 3921 - 24</p>
-          <p><strong>Fax :</strong> (021) 590 1274, 590 3937</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <div class="footer-bottom">
     <div class="container">
-        <div class="pull-left"> Copyright 2018 © PT Bando Indonesia. All right reserved.</div>
+        <div class="text-center"> Copyright 2018 © PT Bando Indonesia. All right reserved.</div>
     </div>
   </div>
 </footer>
